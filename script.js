b1c08228c0560530b6cd89ee2a4673023df86510
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2022-09-15 16:04:44 +0700

    Implement CSS grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const getId = id => document.getElementById(id);
+const qSA = q => document.querySelectorAll(q);
+const gId = id => document.getElementById(id);
 
 const prevSmt = () => {
 	const smt = getId("smt");
@@ -11,3 +12,40 @@ const nextSmt = () => {
 	smt.innerHTML < 8 && smt.innerHTML++;
 	smt.innerHTML = smt.innerHTML;
 }
+
+const hambEnter = () => {
+	span.forEach(e => {
+		e.style.background = "#3ff";
+	});
+}
+
+const hambLeave = () => {
+	span.forEach(e => {
+		e.style.background = "#000";
+	});
+}
+
+const hambClick = e => {
+	if (e.checked){
+		hamb.style.transform = "translate(100%,200%)";
+		leftAside.style.transform = "initial";
+	} else {
+		hamb.style.transform = "initial";
+		leftAside.style.transform = "translateX(-100%)";
+	};
+}
+
+const hamb = gId("hamb");
+const span = qSA("header span");
+const hambBtn = qSA("header input");
+const leftAside = gId("left-aside");
+
+
+
+
+((i=.2) => {
+	span.forEach(e => {
+		e.style.transition = `all ${i}s`;
+		i += .5;
+	});
+})();
